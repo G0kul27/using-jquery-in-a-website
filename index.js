5707ec80912d4f5bdc421d0f8294711353695260
Gokul S <gokul270520@gmail.com>
2022-04-11 10:20:16 +0530

    jquery

---
+++ index.js
@@ -0,0 +1,111 @@
+
+                    // adding a class name for title via jquery
+            
+            $("h2").text("about web development");
+            $("h2").addClass("big-heading");
+
+
+             // changing the font-size and color for h3 using jquery
+            $("h3").css("fontSize","3rem");
+            $("h3").css("textShadow","0 0 4px #49FF00");
+
+            // changing the font-size of h1 using jquery
+
+
+            $("h1").css("fontSize","2.5rem");
+            $("h1").css("textShadow","1px 1px 1px #00FFC6");
+
+
+            // changing the paragraph size using jquery
+
+            $("p").css("fontSize","1.25rem");
+
+            // changing the size of list tags using jquery
+
+            $("li").css("fontSize","1.25rem");
+
+
+            // changing the background color of part-1  on over using jquery
+
+
+            $("#web-dev").on("mouseover",function()
+            {
+                $("#web-dev").css("backgroundColor","#F7E9D7").css("padding","20px");
+            });
+
+
+
+            
+            // changing the background color of part-2  on over using jquery
+
+
+            $("#exact-web-dev").on("mouseover",function()
+            {
+                $("#exact-web-dev").css("backgroundColor","#F7F5F2").css("padding","20px");
+            });
+
+
+
+             // changing the background color of part-3  on over using jquery
+
+
+             $("#com-lang").on("mouseover",function()
+             {
+                 $("#com-lang").css("backgroundColor","#F7E9D7").css("padding","20px");
+             });
+
+
+
+
+             // changing the background color of part-4 on over using jquery
+
+
+             $("#types-web-dev").on("mouseover",function()
+             {
+                 $("#types-web-dev").css("backgroundColor","#F7F5F2").css("padding","20px");
+             });
+
+
+
+              // changing the background color of part-5 on over using jquery
+
+
+              $("#front-end").on("mouseover",function()
+              {
+                  $("#front-end").css("backgroundColor","#F7E9D7").css("padding","20px");
+              });
+
+
+
+              
+              // changing the background color of part-6 on over using jquery
+
+
+              $("#jobs-front-end").on("mouseover",function()
+              {
+                  $("#jobs-front-end").css("backgroundColor","#F7F5F2").css("padding","20px");
+              });
+
+
+
+               
+              // changing the background color of part-7 on over using jquery
+
+
+              $("#back-end").on("mouseover",function()
+              {
+                  $("#back-end").css("backgroundColor","#F7E9D7").css("padding","20px");
+              });
+
+
+
+
+              // changing the background color of part-8 on over using jquery
+
+
+              $("#full-stack").on("mouseover",function()
+              {
+                  $("#full-stack").css("backgroundColor","#F7F5F2").css("padding","20px");
+              });
+
+           
